b9b10dd6a8fa455cecd80a2221d68903b1e916f4
Andres Vasquez <andres.vasquez.a@hotmail.com>
2016-04-06 19:13:12 -0400

    Ejemplo2. Login

---
+++ Login/app/src/main/java/android/curso/login/MainActivity.java
@@ -0,0 +1,81 @@
+package android.curso.login;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText txtUsuario;
+    private EditText txtPassword;
+    private Button btnEnviar;
+    private TextView txtResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Comentado porque no necesitamos el action bar
+        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);*/
+
+        //Comentado porque no necesitamos el boton flotante
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+
+        //Vinculamos las variables con los IDs de la interfaz
+        txtUsuario=(EditText)findViewById(R.id.txtUsuario);
+        txtPassword=(EditText)findViewById(R.id.txtPassword);
+        btnEnviar=(Button)findViewById(R.id.btnEnviar);
+        txtResultado=(TextView)findViewById(R.id.txtResultado);
+
+        //Evento de click en el botón
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String campo_usuario = txtUsuario.getText().toString();
+                String campo_password = txtPassword.getText().toString();
+
+                if (campo_usuario.compareTo("hola") == 0 && campo_password.compareTo("mundo") == 0)
+                    txtResultado.setText("Login aceptado");
+                else
+                    txtResultado.setText("Login fallido");
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
